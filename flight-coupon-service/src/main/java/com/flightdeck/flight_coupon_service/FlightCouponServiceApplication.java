f5143f3ae0914620f28500c632615b951cf7bce8
Gowtham Reddy <gowthammanikanta07@gmail.com>
2026-03-05 05:48:11 +0530

    Folder Structure

---
+++ flight-coupon-service/src/main/java/com/flightdeck/flight_coupon_service/FlightCouponServiceApplication.java
@@ -0,0 +1,13 @@
+package com.flightdeck.flight_coupon_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightCouponServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightCouponServiceApplication.class, args);
+	}
+
+}
---
+++ flight-coupon-service/src/test/java/com/flightdeck/flight_coupon_service/FlightCouponServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flightdeck.flight_coupon_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightCouponServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
