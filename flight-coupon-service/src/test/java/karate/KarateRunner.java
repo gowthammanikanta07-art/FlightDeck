cc581af16d04f6f12a6ef118a434c12cbbaa2bb8
Gowtham Reddy <gowthammanikanta07@gmail.com>
2026-04-15 02:05:11 +0530

    Fix pipeline breaking change

--- flight-coupon-service/src/test/java/karate/KarateRunner.java
+++ flight-coupon-service/src/test/java/karate/KarateRunner.java
@@ -6,6 +6,6 @@ class KarateRunner {
 
     @Karate.Test
     Karate testAll() {
-        return Karate.run()..outputCucumberJson(true).relativeTo(getClass());
+        return Karate.run().outputCucumberJson(true).relativeTo(getClass());
     }
 }
